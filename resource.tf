41ef09de5cffe9628c7a44098b0abcb3dfdfbdcc
Aakanksh-Mohan <aakanksh2510@gmail.com>
2025-07-17 17:04:49 +0000

    storage

--- resource.tf
+++ resource.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-case1"
   location = "East US"
 }
+resource "azurerm_storage_account" "storage" {
+  name                     = "storageDA2510"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
